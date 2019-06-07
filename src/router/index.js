fe63b5cdac31511f808d60533137e21eb31725a3
raulrp3 <raul.ramirez@escuelaestech.es>
2019-06-07 13:52:51 +0200

    proyecto base

---
+++ src/router/index.js
@@ -0,0 +1,73 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import firebase from 'firebase'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+let router = new Router({
+
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: {
+        requiredAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiredGuest: true
+      }
+    },
+    {
+      path: '/allow',
+      name: 'allowEmail',
+      meta: {
+        requiredAuth: true
+      }
+    },
+    {
+      path: '/show',
+      name: 'showAllowed',
+      meta: {
+        requiredAuth: true
+      }
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiredAuth)) {
+    if (!firebase.auth().currentUser) {
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    } else {
+      next()
+    }
+  } else if (to.matched.some(record => record.meta.requiredGuest)) {
+    if (firebase.auth().currentUser) {
+      next({
+        path: '/',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
